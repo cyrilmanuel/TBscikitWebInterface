dae3d2b2e1428196204c13837e7e6769481cf2fc
cyrilmanuel <cyriljeanneret@gmail.com>
2017-06-20 00:25:20 +0200

    Add forms for params classificator

---
+++ static/JS/ShapeGenerator.js
@@ -0,0 +1,63 @@
+(function () {
+
+    function ShapeGenerator(name) {
+        this.Container_constructor();
+        this.name = name;
+        this.isStateClick = true;
+        this.setup();
+    }
+
+    var p = createjs.extend(ShapeGenerator, createjs.Container);
+
+
+    p.setup = function () {
+        var text = new createjs.Text(this.name, "20px Arial", "#000");
+        text.y = -7;
+        text.textAlign = "center";
+
+
+        var background = new createjs.Shape();
+        background.graphics.beginFill("#c7f4b7").drawCircle(0, 0, 50);
+        this.addChild(background, text);
+
+        this.on("click", this.handleClick);
+        this.on("rollover", this.handleRollOver);
+        this.on("rollout", this.handleRollOver);
+        this.on("pressup", this.handlePressUp);
+        this.on("pressmove", this.handlePressMove);
+
+        this.cursor = "arrow";
+
+        this.mouseChildren = false;
+
+        this.offset = Math.random() * 10;
+        this.count = 0;
+
+        this.x = this.y = 50;
+    };
+
+    p.handleClick = function (event) {
+        //shareRenderFunc();
+        if (this.isStateClick) {
+         shareRenderFormShape(getData()[this.name]);
+        }
+    };
+
+    p.handleRollOver = function (event) {
+        this.alpha = event.type == "rollover" ? 0.4 : 1;
+    };
+
+    p.handlePressUp = function (event) {
+         this.isStateClick = true;
+    };
+
+
+    p.handlePressMove = function (event) {
+        event.target.x = event.stageX;
+        event.target.y = event.stageY;
+        this.isStateClick = false;
+    };
+
+
+    window.ShapeGenerator = createjs.promote(ShapeGenerator, "Container");
+}());
--- static/JS/test.js
+++ static/JS/test.js
@@ -11,22 +11,61 @@ class ButtonList extends React.Component {
         let rows = items.map(
             item => {
                 return (
-                     <li className="collection-item" onClick={this.onClick} id={item}
-                            key={item}>{item}</li>
+                    <li className="collection-item" onClick={this.onClick} id={item}
+                        key={item}>{item}</li>
                 );
             }
         );
-
         return (
-             <ul className="collection">{rows}</ul>
+            <ul className="collection">{rows}</ul>
         );
     }
 }
 
-window
-    .shareRenderFunc5 = function (tab) {
+window.shareRenderButtonList = function (tab) {
     ReactDOM.render(
         <ButtonList items={tab}/>,
         document.getElementById('app')
+    );
+};
+
+// ------------------------------------------- Show Form ---------------------------------------
+
+class FormShape extends React.Component {
+    render() {
+        let dict = this.props.dict || {}
+        let valueParams = this.props.valueParam || []
+        let items = this.props.nameParam || []
+
+        let label = Object.keys(dict).map(name => {
+                if (dict[name] == true) {
+                    return (
+                        <div>{name} : true</div>
+                    );
+                } else {
+                    return (
+                        <div>{name} : {dict[name]}</div>
+                    );
+                }
+            }
+        );
+        return (
+            <div>{label}</div>
+        );
+    }
+}
+
+window.shareRenderFormShape = function (dictParamsClassifier) {
+
+    var nameParams = [];
+    var valueParams = [];
+    for (var key in dictParamsClassifier) {
+        nameParams.push(key);
+        valueParams.push(dictParamsClassifier[key]);
+    }
+
+    ReactDOM.render(
+        <FormShape nameParam={nameParams} valueParam={valueParams} dict={dictParamsClassifier}/>,
+        document.getElementById('formClassificator')
     );
 };
