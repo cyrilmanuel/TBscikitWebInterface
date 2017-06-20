90af863c6587e8c6479952b05467199a2b1aaf32
cyrilmanuel <cyriljeanneret@gmail.com>
2017-06-20 18:23:12 +0200

    Update setup.py dep

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='interfaceMl',
-    packages=['interfaceMl'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
+        'numpy',
+        'aniso8601',
+        'click',
+        'Flask-RESTful',
+        'itsdangerous',
+        'Jinja2',
+        'MarkupSafe',
+        'nose',
+        'npm',
+        'numpy',
+        'optional-django',
+        'python-dateutil',
+        'pytz',
+        'scikit-learn',
+        'scipy',
+        'six',
+        'Werkzeug',
     ],
 )
