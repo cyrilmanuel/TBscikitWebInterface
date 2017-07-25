c7d7d747ae22d78891d8119518cd9f0c77a0f95a
cyrilmanuel <cyriljeanneret@gmail.com>
2017-07-25 15:55:16 +0200

    fix requirement

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
-        'flask',
+        'flask>=0.12.2',
         'Flask-RESTful',
         'scikit-learn',
         'numpydoc',
         'scikit-plot',
+        'nose',
+        'mlxtend',
     ],
     setup_requires=[
         'pytest-runner',
